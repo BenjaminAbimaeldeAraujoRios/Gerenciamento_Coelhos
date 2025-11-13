b7b0164bc2cf07a678976d193dcab9a6755e245e
BenjaminAbimaeldeAraujoRios <benjamin.2023005610@aluno.iffar.edu.br>
2025-11-13 11:32:50 -0300

    Adicoes no frontend

--- back_end/server.js
+++ back_end/server.js
@@ -6,7 +6,6 @@ const Database = require('./database');
 const { rotas } = require("./controller/admin"); 
 
 const app = express();
-// use a secret to enable signed cookies
 app.use(cookieParser(process.env.COOKIE_SECRET || 'gc-secret-dev'));
 
 Database.conectar(
@@ -15,15 +14,15 @@ Database.conectar(
 
 app.use(express.json());
 
-// Serve static files from absolute path to avoid 404 when cwd != repo root
+
 const frontendDir = path.join(__dirname, '..', 'front_end');
 app.use("/front_end", express.static(frontendDir));
-// Simple request logger for debugging
+
 app.use((req, res, next) => {
   console.log('REQ', req.method, req.url);
   next();
 });
-// quick health endpoint
+
 app.get('/health', (req, res) => res.json({ok: true}));
 rotas(app);
 
